afdba6fbfa6392c6448cade32390dca51c42a754
TarushJain <f20221631@hyderabad.bits-pilani.ac.in>
2026-01-14 16:16:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -281,7 +281,7 @@ const products = [
      //Sets
  { id: 33, name: "Onyx Set", category: "sets", image: "Apple steel-p.jpg", sizes: ["600/1500/1700","1500/1700/3500","1700/3500/5000"], prices: { "600/1500/1700":315,"1500/1700/3500":425,"1700/3500/5000":555}, retailPrices: {"600/1500/1700":320,"1500/1700/3500":435,"1700/3500/5000":570} },
  //{ id: 34, name: "Casa Set", category: "sets", image: "Apple steel-p.jpg", sizes: ["2400/3000/5000"], prices: { "2400/3000/5000":545}, retailPrices: {"2400/3000/5000":560} },
- { id: 35, name: "Croma Set", category: "sets", image: "Apple steel-p.jpg", sizes: ["1500/2000/3000","3000/4500/5500"], prices: { "1500/2000/3000":435,"3000/4500/5500":765}, retailPrices: {"1500/2000/3000":450,"3000/4500/5500":785} },
+ { id: 35, name: "Croma Set", category: "sets", image: "Apple steel-p.jpg", sizes: ["1500/2000/3000","2000/3000/4500","3000/4500/5500"], prices: { "1500/2000/3000":435,"2000/3000/4500":565,"3000/4500/5500":765}, retailPrices: {"1500/2000/3000":450,"2000/3000/4500":580,"3000/4500/5500":785} },
 // { id: 36, name: "Cresto Set", category: "sets", image: "Apple steel-p.jpg", sizes: ["2400/3000/5000"], prices: { "2400/3000/5000":710}, retailPrices: {"2400/3000/5000":740} },
  //{ id: 37, name: "Croma SS Set", category: "sets", image: "Apple steel-p.jpg", sizes: ["2000/3000/4500"], prices: { "2000/3000/4500":700}, retailPrices: {"2000/3000/4500":720} },
  //{ id: 52, name: "Zigma SS Set", category: "sets", image: "Apple steel-p.jpg", sizes: ["2000/3000/4500"], prices: { "2000/3000/4500":700}, retailPrices: {"2000/3000/4500":720} }
@@ -1350,6 +1350,7 @@ function applyCartTax() {
         updateCart();
     }
 }
+
 
 
 
